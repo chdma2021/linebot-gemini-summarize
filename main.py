f6ec06bcc95a049c39a696e79d34918ccc154c50
CHDMA <chdma2021@gmail.com>
2024-12-13 19:27:20 +0800

    fix bug of code
    TypeError: can only concatenate str (not "NoneType") to str

--- main.py
+++ main.py
@@ -332,9 +332,10 @@ def handle_message(event):
     try:
        #先取得使用者 Display Name (也就是顯示的名稱)
        profile = line_bot_api.get_profile(user_id)
-       print('user display name = ' + profile.display_name) #記錄使用者名稱
-       print('user picture_url = ' + profile.picture_url) #大頭貼網址
-       print('user status_message = ' + profile.status_message) #狀態消息
+       if not profile.display_name.strip() == "":
+           print('user display name = ' + profile.display_name) #記錄使用者名稱
+       if not profile.picture_url.strip() == "":
+           print('user picture_url = ' + profile.picture_url) #大頭貼網址
     except LineBotApiError as e:
         print(e.status_code)
         print(e.request_id)
